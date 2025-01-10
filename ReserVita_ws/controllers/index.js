4973c9d3f695eb9ad4a825e3ca74c8ccc2f1c50d
Andres Vallejo <andresjavierv50@gmail.com>
2025-01-10 17:50:04 -0500

    ReserVita-Andres-availability

--- ReserVita_ws/controllers/index.js
+++ ReserVita_ws/controllers/index.js
@@ -1,9 +1,11 @@
 const usersController = require('./usersController');
 const roomsController = require('./roomsController');
 const resourcesController = require('./resourcesController');
+const availabilityController = require('./availabilityController');
 
 module.exports = {
     usersController,
     roomsController,
-    resourcesController
+    resourcesController,
+    availabilityController
 };
